a6b4020295ba6ed4185431f0315febfddeecf22f
Ivy Xu <xyan21418@gmail.com>
2024-10-02 12:38:52 +0300

    exercise 1.14 done

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,49 @@
 import { useState } from 'react'
 
+const DisplayAnecdote = ({ anecdotes, points, index }) => {
+  return (
+    <div>
+      <p>{anecdotes[index]}</p>
+      <p>has {points[index]} votes</p>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const VotingResult = ({ points, anecdotes }) => {
+  let voted = false;
+  let maxPoint = 0
+  let winnerIndex = 0
+  for (let i = 0; i < 8; i++) {
+    if (voted === false && points[i] !== 0)
+      voted = true
+    if (maxPoint < points[i]) {
+      maxPoint = points[i]
+      winnerIndex = i
+    }
+  }
+
+  if (voted === false) {
+    return (
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <p>No votes yet</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <DisplayAnecdote anecdotes={anecdotes} points={points} index={winnerIndex} />
+    </div>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,35 +55,36 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+ 
   const [selected, setSelected] = useState(0)
-  const initialPoints = Array(8).fill(0);
+  const initialPoints = Array(8).fill(0)
   const [points, setpoints] = useState(initialPoints)
 
-  const hanleClick = () => {
+  const hanleNext = () => {
     let randomNumber
     do {
-      randomNumber = Math.floor(Math.random() * 8);
+      randomNumber = Math.floor(Math.random() * 8)
     } while (randomNumber === selected)
-    console.log(randomNumber)
+    console.log("randomNumber:", randomNumber)
     setSelected(randomNumber)
   }
 
   const handleVote = () => {
     const updatedPoints = [...points]
     updatedPoints[selected] += 1
-    console.log(updatedPoints)
+    console.log("updatedPoints:", updatedPoints)
     setpoints(updatedPoints)
   }
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
+      <h1>Anecdote of the day</h1>
+      <DisplayAnecdote anecdotes={anecdotes} points={points} index={selected} />
       <div>
-        <button onClick={handleVote}>vote</button>
-        <button onClick={hanleClick}>next anecdote</button>
+        <Button handleClick={handleVote} text="vote" />
+        <Button handleClick={hanleNext} text="next anecdote" />
       </div>
+      <VotingResult points={points} anecdotes={anecdotes}/>
     </div>
   )
 }
