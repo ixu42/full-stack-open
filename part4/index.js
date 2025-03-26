333d03852fb5a11a80519278d97d5d245ad8565c
Ivy Xu <xyan21418@gmail.com>
2025-03-26 15:01:38 +0200

    refactored the app into separate modules (exercise 4.2)

--- part4/index.js
+++ part4/index.js
@@ -1,48 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const db_password = process.argv[2]
-
-const mongoUrl = `mongodb+srv://fullstack:${db_password}@cluster0.gwpwk.mongodb.net/bloglist?retryWrites=true&w=majority&appName=Cluster0`
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello from the blogs app!</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
