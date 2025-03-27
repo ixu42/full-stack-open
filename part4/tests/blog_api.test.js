b579386cde0e07669b8cea30ccbb5b789f4e1673
Ivy Xu <xyan21418@gmail.com>
2025-03-27 16:06:51 +0200

    wrote a test to verify the unique identifier property of blog posts is named id, not _id (exercise 4.9)

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -22,7 +22,23 @@ test('blogs return in correct amount and in JSON', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('blogs have id instead of _id', async () => {
+  response = await api.get('/api/blogs')
+
+  const blogs = response.body
+  assert.ok(Array.isArray(blogs), 'Response should be an array')
+
+  if (blogs.length > 0) {
+    const blog = blogs[0]
+    assert.ok(blog.id, 'Blog should have an id property')
+    assert.ok(typeof blog.id, String, 'id property of Blog should be string')
+    assert.ok(!blog._id, 'Blog should not have an _id property')
+  } else {
+    asssert.fail('No blogs found in the test database')
+  }
 })
 
 after(async () => {
