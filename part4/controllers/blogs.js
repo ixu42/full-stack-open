b98e9334935c2d2c559e30b8d592b403827ff644
Ivy Xu <xyan21418@gmail.com>
2025-03-29 21:09:42 +0200

    now blog can be deleted only by the user who added it (exercise 4.21)

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -34,11 +34,19 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-  const blog = await Blog.findByIdAndDelete(request.params.id)
-
+  const blog = await Blog.findById(request.params.id)
   if (!blog) {
     return response.status(404).json({ error: 'Blog not found' })
   }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id)
+    return response.status(401).json({ error: 'Invalid token' })
+
+  if (blog.user.toString() !== decodedToken.id)
+    return response.status(403).json({ error: 'You do not have permission to delete this blog' })
+
+  await Blog.findByIdAndDelete(request.params.id)
 
   response.status(204).end()
 })
