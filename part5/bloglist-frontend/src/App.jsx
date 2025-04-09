03e4a56de6d589f3bac147bcc09d12d501187c5e
Ivy Xu <xyan21418@gmail.com>
2025-04-09 16:42:01 +0300

    sorted blogs list by number of likes (exercise 5.10)

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -112,7 +112,7 @@ const App = () => {
       <Togglable buttonLabel='new note' ref={blogFormRef}>
         <BlogForm createBlog={addBlog} />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
       )}
     </div>
