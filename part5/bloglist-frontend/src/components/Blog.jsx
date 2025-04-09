4774e750f6eee709dddf8e378d33b39da468e472
Ivy Xu <xyan21418@gmail.com>
2025-04-09 12:00:38 +0300

    added a button to blog, which controls whether details are shown (exercise 5.7); 5.6 done earlier.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  if (!showDetails) return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleShowDetails}>view</button>
+      </div>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleShowDetails}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>
+  )
+}
 
 export default Blog
