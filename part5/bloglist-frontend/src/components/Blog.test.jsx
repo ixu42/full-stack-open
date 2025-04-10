81b1f2719e9848264996011b48d4adce2bf44cb1
Ivy Xu <xyan21418@gmail.com>
2025-04-10 18:21:45 +0300

    made a test to verify clicking like button twice calls event handler component received as props twice (exercise 5.15)

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -40,9 +40,35 @@ test('renders blog url and likes if clicking view button', async () => {
 
   render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
   const user = userEvent.setup()
-  const button = screen.getByText('view')
-  await user.click(button)
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
 
   screen.getByText('https://example.com')
   screen.getByText('likes 5')
+})
+
+test('clicking like button twice calls event handler twice', async () => {
+  const blog = {
+    "title": "Testing React apps",
+    "author": "Foo",
+    "url": "https://example.com",
+    "likes": 0,
+    user: {
+      name: "Bar"
+    }
+  }
+
+  const mockUpdateBlog = vi.fn()
+  const mockRemoveBlog = vi.fn()
+
+  render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockUpdateBlog.mock.calls).toHaveLength(2)
 })
