73524238893dace482dd8b5c61dc272d45065875
Ivy Xu <xyan21418@gmail.com>
2025-04-10 18:28:28 +0300

    refactoring test

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,73 +2,55 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders blog title and author but not url or likes', () => {
-  const blog = {
-    "title": "Testing React apps",
-    "author": "Foo",
-    "url": "https://example.com",
-    "likes": 5
-  }
+describe('<Blog />', () => {
+  let blog
+  let mockUpdateBlog
+  let mockRemoveBlog
 
-  const mockUpdateBlog = vi.fn()
-  const mockRemoveBlog = vi.fn()
-
-  render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
-
-  screen.getByText('Testing React apps', { exact: false })
-  screen.getByText('Foo', { exact: false })
-  const elemUrl = screen.queryByText('https://example.com')
-  const elemLikes = screen.queryByText('likes 5')
-  expect(elemUrl).toBeNull()
-  expect(elemLikes).toBeNull()
-})
-
-
-test('renders blog url and likes if clicking view button', async () => {
-  const blog = {
-    "title": "Testing React apps",
-    "author": "Foo",
-    "url": "https://example.com",
-    "likes": 5,
-    user: {
-      name: "Bar"
+  beforeEach(() => {
+    blog = {
+      title: 'Testing React apps',
+      author: 'Foo',
+      url: 'https://example.com',
+      likes: 5,
+      user: { name: 'Bar' }
     }
-  }
+    mockUpdateBlog = vi.fn()
+    mockRemoveBlog = vi.fn()
+  })
 
-  const mockUpdateBlog = vi.fn()
-  const mockRemoveBlog = vi.fn()
+  test('renders blog title and author but not url or likes', () => {
+    render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
 
-  render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
-  const user = userEvent.setup()
-  const viewButton = screen.getByText('view')
-  await user.click(viewButton)
+    screen.getByText('Testing React apps', { exact: false })
+    screen.getByText('Foo', { exact: false })
 
-  screen.getByText('https://example.com')
-  screen.getByText('likes 5')
-})
+    expect(screen.queryByText('https://example.com')).toBeNull()
+    expect(screen.queryByText('likes 5')).toBeNull()
+  })
 
-test('clicking like button twice calls event handler twice', async () => {
-  const blog = {
-    "title": "Testing React apps",
-    "author": "Foo",
-    "url": "https://example.com",
-    "likes": 0,
-    user: {
-      name: "Bar"
-    }
-  }
+  test('renders blog url and likes if clicking view button', async () => {
+    render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
 
-  const mockUpdateBlog = vi.fn()
-  const mockRemoveBlog = vi.fn()
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
 
-  render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
-  const user = userEvent.setup()
-  const viewButton = screen.getByText('view')
-  await user.click(viewButton)
+    screen.getByText('https://example.com')
+    screen.getByText('likes 5')
+  })
 
-  const likeButton = screen.getByText('like')
-  await user.click(likeButton)
-  await user.click(likeButton)
+  test('clicking like button twice calls event handler twice', async () => {
+    render(<Blog blog={blog} updateBlog={mockUpdateBlog} removeBlog={mockRemoveBlog} />)
 
-  expect(mockUpdateBlog.mock.calls).toHaveLength(2)
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockUpdateBlog.mock.calls).toHaveLength(2)
+  })
 })
