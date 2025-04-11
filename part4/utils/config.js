f0c56ffce93bc6a4df874bfdd8e9fe48b93475db
Ivy Xu <xyan21418@gmail.com>
2025-04-11 11:05:11 +0300

    separated db for dev, test, prod servers

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = {
+  development: process.env.MONGODB_URI_DEV,
+  test: process.env.MONGODB_URI_TEST,
+  production: process.env.MONGODB_URI_PROD,
+}[process.env.NODE_ENV || 'development']
 
 module.exports = {
   PORT,
