bce964ca2124dca154e3d6a58d29bb2f8b1aefb5
Ivy Xu <xyan21418@gmail.com>
2025-04-12 17:31:42 +0300

    added tests for successful and failed logins; added POST /api/testing/reset endpoint to clear users and blogs in db before each test (exercise 5.18)

--- part5/blogs-e2e/tests/blog_app.spec.js
+++ part5/blogs-e2e/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+import { loginWith } from './helper'
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ request, page }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        "username": "testuser",
+        "name": "testname",
+        "password": "securepassword"
+      }
+    })
     await page.goto('/')
   })
 
@@ -10,5 +19,21 @@ describe('Blog app', () => {
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'testuser', 'securepassword')
+      await expect(page.getByText('testname logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'testuser', 'wrong')
+
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('border', '1px solid rgb(255, 0, 0)')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
   })
 })
