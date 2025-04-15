09b191ce0b551b5151c4085e554b406b6760e11b
Ivy Xu <xyan21418@gmail.com>
2025-04-15 16:19:29 +0300

    added tests for anecdoteReducer

---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.test.js
@@ -0,0 +1,48 @@
+import anecdoteReducer from './anecdoteReducer'
+import deepFreeze from 'deep-freeze'
+
+describe('anecdoteReducer', () => {
+  test('votes added by 1 with action VOTE', () => {
+    const state = [
+      {
+        content: 'the app state is in redux store',
+        id: 1,
+        votes: 0
+      }
+    ]
+    const action = {
+      type: 'VOTE',
+      payload: {
+        id: state[0].id,
+      }
+    }
+
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
+
+    expect(newState).toHaveLength(1)
+    expect(newState).toContainEqual({
+        content: 'the app state is in redux store',
+        id: 1,
+        votes: 1
+      })
+  })
+
+  test('returns new state with action NEW_ANECDOTE', () => {
+    const state = []
+    const action = {
+      type: 'NEW_ANECDOTE',
+      payload: {
+        content: 'the app state is in redux store',
+        id: 1,
+        votes: 0
+      }
+    }
+
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
+
+    expect(newState).toHaveLength(1)
+    expect(newState).toContainEqual(action.payload)
+  })
+})
