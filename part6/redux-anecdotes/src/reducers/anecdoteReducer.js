a1eb2dff434a7571cdfab919650eb0acf88984f5
Ivy Xu <xyan21418@gmail.com>
2025-04-17 16:30:40 +0300

    also refactored anecdote creation to use async action creator (Ex. 6.17)

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -13,22 +13,29 @@ const anecdoteSlice = createSlice({
           : anecdote
       )
     },
-    createAnecdote(state, action) {
-      state.push(action.payload)
-    },
     setAnecdotes(state, action) {
       return action.payload
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload)
     }
   }
 })
 
-export const { voteAnecdote, createAnecdote, setAnecdotes } =
+export const { voteAnecdote, setAnecdotes, appendAnecdote } =
 anecdoteSlice.actions
 
 export const initAnecdotes = () => {
   return async dispatch => {
     const anecdotes = await anecdoteService.getAll()
     dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
   }
 }
 
