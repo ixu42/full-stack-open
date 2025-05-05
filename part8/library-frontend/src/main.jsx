06214337ed588c130424866589af1b949a2e187a
Ivy Xu <xyan21418@gmail.com>
2025-05-05 23:20:23 +0300

    implement Authors view to show details of all authors (ex. 8.8); format the code

--- part8/library-frontend/src/main.jsx
+++ part8/library-frontend/src/main.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
-);
+  </ApolloProvider>
+)
