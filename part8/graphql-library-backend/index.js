cfb9882e451f4a320f14d71c34efc5b167b82172
Ivy Xu <xyan21418@gmail.com>
2025-05-05 13:57:31 +0300

    modify the query allBooks so that a user can give an optional parameter genre too (ex. 8.5); format code with prettier

--- part8/graphql-library-backend/index.js
+++ part8/graphql-library-backend/index.js
@@ -4,27 +4,27 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 let authors = [
   {
     name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
+    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
+    born: 1952
   },
   {
     name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
     born: 1963
   },
   {
     name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
     born: 1821
   },
-  { 
+  {
     name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e'
   },
-  { 
+  {
     name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
+    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e'
+  }
 ]
 
 /*
@@ -39,58 +39,58 @@ let authors = [
  * Spanish:
  * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
  * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
+ */
 
 let books = [
   {
     title: 'Clean Code',
     published: 2008,
     author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring']
   },
   {
     title: 'Agile software development',
     published: 2002,
     author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
     genres: ['agile', 'patterns', 'design']
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
     author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring']
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
     author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring', 'patterns']
-  },  
+  },
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
     author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
     genres: ['refactoring', 'design']
   },
   {
     title: 'Crime and punishment',
     published: 1866,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
     genres: ['classic', 'crime']
   },
   {
     title: 'Demons',
     published: 1872,
     author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
     genres: ['classic', 'revolution']
-  },
+  }
 ]
 
 const typeDefs = `
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -122,25 +122,34 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      return books.filter(book => book.author === args.author)
+      if (!args.author && !args.genre) return books
+      if (args.author && !args.genre) {
+        return books.filter((book) => book.author === args.author)
+      }
+      if (!args.author && args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre))
+      }
+      return books.filter(
+        (book) =>
+          book.author === args.author && book.genres.includes(args.genre)
+      )
     },
     allAuthors: () => authors
   },
   Author: {
     bookCount: (root) => {
-      return books.filter(book => book.author === root.name).length
+      return books.filter((book) => book.author === root.name).length
     }
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 })
 
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: 4000 }
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
