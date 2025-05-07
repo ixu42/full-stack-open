55ef48a89d4d2e4dd8430aaa042c8bb20d499561
Ivy Xu <xyan21418@gmail.com>
2025-05-07 16:39:11 +0300

    refactor App component; redirect to books view after login

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -25,40 +25,35 @@ const App = () => {
     client.resetStore()
   }
 
-  if (!token) {
-    return (
-      <div>
-        <div>
-          <button onClick={() => setPage('authors')}>authors</button>
-          <button onClick={() => setPage('books')}>books</button>
-          <button onClick={() => setPage('login')}>login</button>
-        </div>
-
-        <Notify errorMessage={error} />
-        <Authors show={page === 'authors'} setError={notify} />
-        <Books show={page === 'books'} />
-        <LoginForm
-          show={page === 'login'}
-          setToken={setToken}
-          setError={notify}
-        />
-      </div>
-    )
-  }
+  const loggedIn = Boolean(token)
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-        <button onClick={logout}>logout</button>
+        {loggedIn ? (
+          <>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+        ) : (
+          <button onClick={() => setPage('login')}>login</button>
+        )}
       </div>
 
       <Notify errorMessage={error} />
       <Authors show={page === 'authors'} setError={notify} />
       <Books show={page === 'books'} />
-      <NewBook show={page === 'add'} setError={notify} />
+      {loggedIn && <NewBook show={page === 'add'} setError={notify} />}
+      {!loggedIn && (
+        <LoginForm
+          show={page === 'login'}
+          setToken={setToken}
+          setPage={setPage}
+          setError={notify}
+        />
+      )}
     </div>
   )
 }
--- part8/library-frontend/src/components/LoginForm.jsx
+++ part8/library-frontend/src/components/LoginForm.jsx
@@ -3,7 +3,7 @@ import { useMutation } from '@apollo/client'
 import PropTypes from 'prop-types'
 import { LOGIN } from '../queries'
 
-const LoginForm = ({ show, setToken, setError }) => {
+const LoginForm = ({ show, setToken, setPage, setError }) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
@@ -18,6 +18,7 @@ const LoginForm = ({ show, setToken, setError }) => {
       const token = result.data.login.value
       setToken(token)
       localStorage.setItem('library-user-token', token)
+      setPage('books')
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [result.data])
@@ -57,6 +58,7 @@ const LoginForm = ({ show, setToken, setError }) => {
 LoginForm.propTypes = {
   show: PropTypes.bool.isRequired,
   setToken: PropTypes.func.isRequired,
+  setPage: PropTypes.func.isRequired,
   setError: PropTypes.func.isRequired
 }
 
