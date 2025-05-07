01ce615c3f86111d98e8809c3152368b403b49e7
Ivy Xu <xyan21418@gmail.com>
2025-05-07 23:13:53 +0300

    filter book list by genre (ex. 8.19)

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = () => {
+  const [selectedGenre, setSelectedGenre] = useState(null)
   const result = useQuery(ALL_BOOKS)
 
   if (result.loading) {
@@ -10,10 +12,24 @@ const Books = () => {
 
   const books = result.data.allBooks
 
+  // get a list of unique genres
+  const genreSet = new Set()
+  books.forEach((book) => {
+    book.genres.forEach((genre) => {
+      genreSet.add(genre)
+    })
+  })
+  const genres = Array.from(genreSet)
+
+  const filteredBooks = selectedGenre
+    ? books.filter((book) => book.genres.includes(selectedGenre))
+    : books
+
   return (
     <div>
       <h2>books</h2>
 
+      {/* table of books */}
       <table>
         <tbody>
           <tr>
@@ -21,15 +37,23 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {filteredBooks.map((book) => (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      {/* buttons to select a specific genre */}
+      {genres.map((genre) => (
+        <button key={genre} onClick={() => setSelectedGenre(genre)}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => setSelectedGenre(null)}>all genres</button>
     </div>
   )
 }
