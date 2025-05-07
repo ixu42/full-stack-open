0c3f9b5fbd58f892bbe9cd1ef519b0a388c7bdd0
Ivy Xu <xyan21418@gmail.com>
2025-05-07 16:28:12 +0300

    implement login and logout functionality

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Notify from './components/Notify'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [error, setError] = useState('')
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
 
   const notify = (message) => {
     setError(message)
@@ -15,12 +19,40 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('login')}>login</button>
+        </div>
+
+        <Notify errorMessage={error} />
+        <Authors show={page === 'authors'} setError={notify} />
+        <Books show={page === 'books'} />
+        <LoginForm
+          show={page === 'login'}
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={logout}>logout</button>
       </div>
 
       <Notify errorMessage={error} />
