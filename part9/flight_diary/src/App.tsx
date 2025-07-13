573109cd1b34360e1f51d2e7f81c9d3e5d46c691
Ivy Xu <xyan21418@gmail.com>
2025-07-13 11:56:02 +0300

    implement functionality to add new diary entry, validation skipped for now (ex. 9.18)

--- part9/flight_diary/src/App.tsx
+++ part9/flight_diary/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
-import { Diary } from './types';
+import type { Diary, Visibility, Weather } from './types';
 import { getAllDiaries } from './diaryService';
 
 function App() {
   const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [date, setDate] = useState<string>('');
+  const [visibility, setVisibility] = useState<Visibility | ''>('');
+  const [weather, setWeather] = useState<'' | Weather>('');
+  const [comment, setComment] = useState<string>('');
 
   useEffect(() => {
     getAllDiaries().then((data) => {
@@ -11,8 +15,61 @@ function App() {
     });
   }, []);
 
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const diaryToAdd = {
+      date: date,
+      visibility: visibility as Visibility,
+      weather: weather as Weather,
+      comment: comment,
+      id: diaries.length + 1
+    };
+    console.log('Adding diary:', diaryToAdd);
+    setDiaries(diaries.concat(diaryToAdd));
+    setDate('');
+    // validate visibility and weather before setting
+    setVisibility('');
+    setWeather('');
+    setComment('');
+    return null;
+  };
+
   return (
     <div>
+      <h2>Add new entry</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          date
+          <input
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility
+          <input
+            value={visibility}
+            onChange={(event) =>
+              setVisibility(event.target.value as Visibility | '')
+            }
+          />
+        </div>
+        <div>
+          weather
+          <input
+            value={weather}
+            onChange={(event) => setWeather(event.target.value as Weather | '')}
+          />
+        </div>
+        <div>
+          comment
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
       <h2>Diary entries</h2>
       {diaries.map((diary) => (
         <div key={diary.id}>
